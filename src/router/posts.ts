d184db65c723a6a5b6eb1df928a3d06f23455461
kimn <a9523023650@gmail.com>
2022-05-22 21:14:03 +0800

    [修改] 移除 ImageService

--- src/router/posts.ts
+++ src/router/posts.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import * as Controller from "../controller";
-import * as Service from "../service";
 import Utils from "../utils";
 
 const router = express.Router();
 
 router.get("/", Utils.catchAsync(Controller.Post.getPosts));
 
-router.post("/", Service.image.handle, Utils.catchAsync(Controller.Post.createPosts));
+router.post("/", Utils.catchAsync(Controller.Post.createPosts));
 
 router.delete("/:id", Utils.catchAsync(Controller.Post.deleteById));
 
