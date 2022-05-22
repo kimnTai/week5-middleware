ade7c3b5c702c31595546bbc9f36fd82ce0a9705
kimn <a9523023650@gmail.com>
2022-05-22 23:25:45 +0800

    [新增] 註解

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,22 @@
 import { Request, Response, NextFunction, RequestHandler, Router } from "express";
 
 class Utils {
+    /**
+     * @description 包裝 RequestHandler，捕捉 Promise.reject
+     * @param {RequestHandler} func
+     * @memberof Utils
+     */
     catchAsync = (func: RequestHandler) => {
         return (req: Request, res: Response, next: NextFunction) => {
             Promise.resolve(func(req, res, next)).catch(next);
         };
     };
 
+    /**
+     * @description 使用 catchAsync 包裝 Router
+     * @param {Router} router
+     * @memberof Utils
+     */
     catchAsyncRouter = (router: Router) => {
         for (const key in router) {
             if (["get", "post", "delete", "patch"].includes(key)) {
