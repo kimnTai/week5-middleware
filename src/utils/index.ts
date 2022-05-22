458e16860b570065ab4c08ac7299896caa0cbb01
kimn <a9523023650@gmail.com>
2022-05-22 23:20:50 +0800

    [新增] catchAsyncRouter

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,23 @@
-import { Request, Response, NextFunction, RequestHandler } from "express";
+import { Request, Response, NextFunction, RequestHandler, Router } from "express";
 
 class Utils {
-    catchAsync(func: RequestHandler) {
+    catchAsync = (func: RequestHandler) => {
         return (req: Request, res: Response, next: NextFunction) => {
             Promise.resolve(func(req, res, next)).catch(next);
         };
-    }
+    };
+
+    catchAsyncRouter = (router: Router) => {
+        for (const key in router) {
+            if (["get", "post", "delete", "patch"].includes(key)) {
+                const method = (<any>router)[key];
+                (<any>router)[key] = (path: any, ...callbacks: any) => {
+                    method.call(router, path, ...callbacks.map((cb: any) => this.catchAsync(cb)));
+                };
+            }
+        }
+        return router;
+    };
 }
 
 export default new Utils();
